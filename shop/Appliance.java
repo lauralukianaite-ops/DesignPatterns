4b79ff2d09a52bb6474281d0a43b45a00108f955
Laura <laura.lukianaite@gmail.com>
2026-04-08 16:10:29 +0300

    Adding Appliance class

--- shop/Appliance.java
+++ shop/Appliance.java
@@ -1,4 +1,15 @@
 package shop;
 
-public class Appliance {
+public class Appliance extends Product{
+    int warranty;
+
+    public Appliance(int warranty, String name, double price){
+        super(name,price);
+        this.warranty=warranty;
+    }
+
+    @Override
+    public void getInfo(){
+        System.out.println("Warranty: " + warranty + " years , name: " + name + ", price" + price);
+    }
 }
