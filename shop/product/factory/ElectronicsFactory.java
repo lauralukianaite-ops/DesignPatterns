7b7bbd481bc673fbfa96c8db6a4a3e509457b8c5
Laura <laura.lukianaite@gmail.com>
2026-04-08 17:52:01 +0300

    Adding ElectronicsFactory class

--- shop/product/factory/ElectronicsFactory.java
+++ shop/product/factory/ElectronicsFactory.java
@@ -1,4 +1,19 @@
 package shop.product.factory;
+import shop.product.Electronics;
+import shop.product.Product;
 
-public class ElectronicsFactory {
+public class ElectronicsFactory extends ProductFactory{
+    private String brand, name;
+    private double price;
+
+    public ElectronicsFactory(String brand, String name, double price){
+        this.brand=brand;
+        this.name=name;
+        this.price=price;
+    }
+
+    @Override
+    public Product createProduct(){
+        return new Electronics(brand,name,price);
+    }
 }
