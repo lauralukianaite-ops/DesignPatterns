518c1729a78e0f95d57c4d53a0b2177ed4f18893
Laura <laura.lukianaite@gmail.com>
2026-04-08 18:25:06 +0300

    Connecting Observer with Inventory

--- shop/inventory/Inventory.java
+++ shop/inventory/Inventory.java
@@ -1,11 +1,14 @@
 package shop.inventory;
+import shop.observer.Observer;
+import shop.observer.Subject;
 import shop.product.Product;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Inventory {
+public class Inventory implements Subject {
     private static Inventory instance;
     private List<Product> products = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
 
     private Inventory(){}
 
@@ -18,9 +21,20 @@ public class Inventory {
 
     public void addProduct(Product p){
         products.add(p);
+        notifyObservers(p);
     }
 
     public List<Product> getProducts(){
         return products;
+    }
+
+    public void addObserver(Observer o){
+        observers.add(o);
+    }
+
+    public void notifyObservers(Product p){
+        for(Observer o : observers){
+            o.update(p);
+        }
     }
 }
