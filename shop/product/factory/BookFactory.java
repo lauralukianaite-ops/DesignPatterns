85beb2943df0ff6e9a13ad27c5fb6b97fc8ff330
Laura <laura.lukianaite@gmail.com>
2026-04-08 17:45:18 +0300

    Adding BookFactory class

--- shop/product/factory/BookFactory.java
+++ shop/product/factory/BookFactory.java
@@ -1,4 +1,19 @@
 package shop.product.factory;
+import shop.product.Book;
+import shop.product.Product;
 
-public class BookFactory {
+public class BookFactory extends ProductFactory{
+    private String name, author;
+    private double price;
+
+    public BookFactory(String name, double price, String author){
+        this.name=name;
+        this.price=price;
+        this.author=author;
+    }
+
+    @Override
+    public Product createProduct() {
+        return new Book(name,price,author);
+    }
 }
